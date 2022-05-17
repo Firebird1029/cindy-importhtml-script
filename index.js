b382bd6a240af87989542d3cf65413aaee81ae16
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2022-05-16 17:27:46 -1000

    remove excess comments, add root req route

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const express = require("express");
-// const jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-
 const app = express();
 const port = 3000;
 
-// app.get("/:zillow1/:zillow2", function (req, res) {
-// 	console.log(req.params);
-// 	res.send("hi");
-// });
+app.get("/", function (req, res) {
+	res.send("hello from brandon");
+});
 
 app.get("*", async function (req, res) {
 	console.log(req.url.substring(req.url.indexOf("homedetails/") + "homedetails/".length, req.url.length));
 	console.log(req.url.substring(1, req.url.length));
-	// (async () => {
-	// 	const response = await fetch(req.url.substring(1, req.url.length));
-	// 	const text = await response.text();
-	// 	const dom = await new JSDOM(text);
-	// 	console.log(dom.window.document.querySelector("h1").textContent);
-	// })();
 
 	try {
 		const $ = await rp({
@@ -42,16 +32,3 @@ app.listen(port, function () {
 
 const rp = require("request-promise");
 const cheerio = require("cheerio");
-
-// rp({
-// 	url: "https://www.zillow.com/...",
-// 	transform: function (html) {
-// 		return cheerio.load(html);
-// 	},
-// })
-// 	.then(($) => {
-// 		console.log($("span[data-testid='zestimate-text']").children().next().text());
-// 	})
-// 	.catch(function (err) {
-// 		console.log(err);
-// 	});
